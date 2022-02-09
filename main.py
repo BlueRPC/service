9ef6fbcdecf619263159a94bed51c2b9f55f2ce7
DrosoCode <internet-site@outlook.com>
2022-02-09 12:16:41 +0100

    add background scan

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ class Worker(worker_pb2_grpc.WorkerServicer):
 
 
 class BluetoothLE(worker_pb2_grpc.BluetoothLEServicer):
+    currentScanResponse = worker_pb2.ScanResult(status=worker_pb2.Status.OK)
+    backgroundScan = True
+
     async def Scan(
         self, request: worker_pb2.DeviceScan, context: grpc.aio.ServicerContext
     ) -> worker_pb2.ScanResult:
@@ -38,9 +41,38 @@ class BluetoothLE(worker_pb2_grpc.BluetoothLEServicer):
                     name=d.name,
                     rssi=d.rssi,
                     manufacturerData=d.metadata.get("manufacturer_data"),
+                    time=round(time.time()),
                 )
             )
         return resp
+
+    def detection_callback(self, d, advertisement_data):
+        self.currentScanResponse.data.append(
+            worker_pb2.Device(
+                mac=d.address,
+                type=worker_pb2.DeviceType.BLE4,
+                name=d.name,
+                rssi=d.rssi,
+                manufacturerData=(d.metadata.get("manufacturer_data") or {}).update(
+                    advertisement_data.manufacturer_data or {}
+                ),
+                time=round(time.time()),
+            )
+        )
+
+    async def ScanBackground(self, request: worker_pb2.DeviceScan, context):
+        scanner = BleakScanner()
+        scanner.register_detection_callback(self.detection_callback)
+        await scanner.start()
+        while self.backgroundScan:
+            await asyncio.sleep(request.time)
+            yield self.currentScanResponse
+            self.currentScanResponse = worker_pb2.ScanResult(
+                status=worker_pb2.Status.OK
+            )
+
+    async def ScanBackgroundStop(self, request, context):
+        self.backgroundScan = False
 
 
 async def serve():
